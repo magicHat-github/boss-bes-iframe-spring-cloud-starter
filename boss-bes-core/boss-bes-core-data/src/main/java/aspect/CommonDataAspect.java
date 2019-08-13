adc746279dd256e7150d12bfc1b654f9cffd3c4f
Bylight <hejiaweicn@163.com>
2019-08-13 10:09:19 +0800

    修改一些参数名和变量名以方便理解

--- boss-bes-core/boss-bes-core-data/src/main/java/aspect/CommonDataAspect.java
+++ boss-bes-core/boss-bes-core-data/src/main/java/aspect/CommonDataAspect.java
@@ -26,12 +26,12 @@ public class CommonDataAspect {
      * 设置切点
      */
     @Pointcut("@annotation(annotation.CommonDataAnnotation)")
-    public void controllerLog() {
+    public void cutPoint() {
     }//签名，可以理解成这个切入点的一个名称
 
-    @Around("controllerLog()")
-    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
-        Object[] args = pjp.getArgs();
+    @Around("cutPoint()")
+    public Object doAround(ProceedingJoinPoint point) throws Throwable {
+        Object[] args = point.getArgs();
         Field[] fields = args[0].getClass().getDeclaredFields();
         System.out.println("请求开始，请求报文：");
         for (Field field: fields) {
@@ -41,11 +41,11 @@ public class CommonDataAspect {
                 field.set(args[0], "切面修改的类别");
             }
         }
-        System.out.println(pjp.getTarget());
+        System.out.println(point.getTarget());
         System.out.println("=========================================");
 
         // result的值就是被拦截方法的返回值
-        Object result = pjp.proceed();
+        Object result = point.proceed();
         logger.info("请求结束，响应报文： " + JSON.toJSONString(result));
         return result;
     }
